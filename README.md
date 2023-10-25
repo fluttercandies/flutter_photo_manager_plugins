727499c412e81078ffad3fef021813ce39451ddd
CaiJingLong <cjl_spy@163.com>
2023-10-25 10:52:37 +0800

    Init commit
    Signed-off-by: CaiJingLong <cjl_spy@163.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plugins for [photo_manager][]
+
+[photo_manager]: https://pub.dev/packages/photo_manager
---
+++ packages/photo_manager_image_provider/lib/photo_manager_image_provider.dart
@@ -0,0 +1,7 @@
+library photo_manager_image_provider;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
